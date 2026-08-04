e7d5d4919a7a0f890d5cba8857e00335a008ba48
Pawan Pramod Shah <9930pawanshah@gmail.com>
2026-08-04 17:25:12 +0530

    Update authMiddlewares.js

--- src/middlwares/authMiddlewares.js
+++ src/middlwares/authMiddlewares.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const signupValidation = (req, res, next) => {
     const signUpSchema = Joi.object({
         name: Joi.string().min(4).max(15).required(),
-        email: Joi.string().required(),
+        email: Joi.string().email().required(),
         password: Joi.string().min(4).max(12).required()
     })
 
